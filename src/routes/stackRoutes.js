a5d11eac883ff318c552b3719f4c33584ee35bc9
eduardo nakai <109884797+eduardonakaidev@users.noreply.github.com>
2022-11-02 21:23:28 -0300

    dasdasda

--- src/routes/stackRoutes.js
+++ src/routes/stackRoutes.js
@@ -6,6 +6,7 @@ import Login from '../pages/Login'
 import Sign from '../pages/Sign'
 import Welcome from '../pages/Welcome'
 
+
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
